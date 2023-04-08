d380d2201e94e565ab6feaabadb622fb64d212ed
wycmichelle11 <79016649+wycmichelle11@users.noreply.github.com>
2023-04-07 21:43:46 -0600

    Dynamic User Completed

--- server/index.js
+++ server/index.js
@@ -18,34 +18,6 @@ app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 
-// app.use(bodyParser.urlencoded({extended: true}));
-
-//getting all jobpostings
-// app.get("/api/get", (req, res)=> {
-//     const sqlSelect="SELECT * FROM job_posting;"
-//     db.query(sqlSelect, (err, result)=> {
-//         res.send(result); //send to our front end
-//     })
-// })
-
-
-
-
-//used only for inserting items
-// app.post('/api/insert', (req, res) => {
-//     const title = req.body.title
-//     const location = req.body.location
-    
-//     const sqlInsert ="INSERT INTO job_posting (title, location) VALUES (?,?);"
-//     db.query(sqlInsert, [title, location], (err, result)=> {
-//         console.log(result);
-//     })
-// });
-
-// app.get('/', (req, res) => {
-    
-// });
-
 app.listen(3001,() => {
     console.log("running on port 3001");
 })
