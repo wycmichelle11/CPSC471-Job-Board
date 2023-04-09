4fb98f0464dd70e00db831c74bcb9d0d35b66385
wycmichelle11 <79016649+wycmichelle11@users.noreply.github.com>
2023-04-09 08:33:02 -0600

    set up delete/edit buttons, but not functional

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState} from 'react';
-import { Link, useLocation } from "react-router-dom";
+import React, { useContext, useEffect, useState} from 'react';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import {AuthContext} from "../context/authContext.js";
 
 const Home = () => {
   const [postings, setPostings] = useState([]);
   
+  const {currentUser} = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const myPosts = useLocation().search;
+  console.log(myPosts);
   useEffect(()=> {
     const fetchData = async () => {
       try{
         console.log("up to here");
-        const res = await axios.get(`/posts${myPosts}`);
+        const res = await axios.get(`/posts`);
         setPostings(res.data);
       }catch (err) {
         console.log(err);
@@ -19,6 +24,15 @@ const Home = () => {
     fetchData();
   }, [myPosts]);
 
+  const handleDelete = async (job_id)=>{
+    try {
+      await axios.delete(`/posts/${job_id}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="home">
         <div className="postings">
@@ -26,6 +40,15 @@ const Home = () => {
             <div className="post" key={post.job_id}>
               <div className="content">
                 <h1>{post.title}</h1>
+                {currentUser.account_id === post.account_id && (
+                  <div className="edit">
+                    <button>Edit</button>
+                    <button onClick={handleDelete(post.job_id)}>Delete</button>
+                  </div>
+                ) }
+
+
+
                 <p>Location: {post.location}</p>
                 <p>Flag: {post.flag}</p>
                 <p>Qualifications: {post.qualification}</p>
