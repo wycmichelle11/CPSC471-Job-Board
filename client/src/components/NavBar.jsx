3f5d60fead59618934db55254eb8b5591bb927d3
wycmichelle11 <79016649+wycmichelle11@users.noreply.github.com>
2023-04-10 12:31:47 -0600

    Add home link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,6 +9,7 @@ const NavBar = () => {
         <div className="navbar">
             <div className="nav-container">
                 <div className="navbar-links">
+                    <Link to="/home" className="navbar-link">Home</Link>
                     <Link to="/home/writepost" className="navbar-link">Add New Posting</Link>
                     <Link to="/home/myaccount" className="navbar-link">{currentUser && currentUser.first_name}</Link>
                     <Link  to="/login" className="navbar-link"  onClick={logout}>Logout</Link>
