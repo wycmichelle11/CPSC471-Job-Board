b68c126eceae0371a29d9bc27f15b2b6f90a4fb8
wycmichelle11 <79016649+wycmichelle11@users.noreply.github.com>
2023-04-11 00:29:08 -0600

    resume works for multi users

--- server/controllers/resumes.js
+++ server/controllers/resumes.js
@@ -1,19 +1,21 @@
 import {db} from "../db.js"
+import jwt from "jsonwebtoken";
 
 export const getResume = (req, res) => {
-    const q = "SELECT * FROM  resume";
-    db.query(q, (err,data) => {
-        if(err) return res.status(500).send(err);
-        return res.status(200).json(data);
-    });
+    const token = req.cookies.access_token;
+    if(!token) return res.status(401).json("Not authenticated!");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+       if (err) return res.status(403).json("Token is not valid");
+        const q = "SELECT * FROM resume r JOIN account a ON r.job_seeker_email = a.email WHERE a.account_id = ?";
+        db.query(q, [userInfo.account_id], (err,data) => {
+            if(err) return res.status(500).send(err);
+            return res.status(200).json(data);
+        });
+    })
 };
 
 export const addResume = (req, res) => {
-    // const token = req.cookies.access_token;
-    // if(!token) return res.status(401).json("Not authenticated!");
-
-    // jwt.verify(token, "jwtkey", (err, userInfo) => {
-    //    if (err) return res.status(403).json("Token is not valid");
     const q = "INSERT INTO resume(`job_seeker_email`, `contact_information`, `education`, `first_name`, `last_name`, `interests`, `additional_information`, `work_experience`) VALUES (?)";  
         const values = [
             req.body.job_seeker_email,
@@ -30,10 +32,6 @@ export const addResume = (req, res) => {
             if (err) return res.status(500).json(err);
             return res.json("Post has been created");
         })
-
-        
-    //  }
-    //  )
 
     
 }
