46bf07875d5228abaf3598bf6486212a67d81a36
Chimbono <ammar.zaghloul55@gmail.com>
2023-04-11 16:56:19 -0600

    Passwords hashed and salted
    32 character salt and SHA256. what more could you want!

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import { db } from "../db.js"
 import jwt from "jsonwebtoken";
-
+import crypto from "crypto";
+const salt = "Umju9wE7LQeYLIyZpFzQuZBI00IgmLXH";
 export const register = (req, res)=> {
 //check existing user
 const q = "SELECT * FROM users WHERE email = ?"
@@ -14,9 +15,11 @@ db.query (q,[req.body.email], (err, data) => {
     const q1 = "INSERT IGNORE INTO company(`name`) VALUES (?)"
     const q2 = "INSERT INTO users(`email`, `password`, `first_name`, `last_name`, `affiliated_company`) VALUES (?)"
     const q3 = "UPDATE users, account SET users.account_id = account.account_id WHERE users.email = account.email"
+    const salted = req.body.password + salt;
+    const hashed = crypto.createHash('sha256').update(salted).digest('hex');
     const values = [
         req.body.email,
-        req.body.password,
+        hashed,
         req.body.first_name,
         req.body.last_name,
         req.body.affiliated_company
@@ -46,7 +49,9 @@ db.query (q,[req.body.email], (err, data) => {
     if (data.length == 0) return res.status(404).json("User not found!");
     
     //check password
-    if (data[0].password != req.body.password) return(res.status(400).json("Incorrect username or password"));
+    const salted = req.body.password + salt;
+    const hashed = crypto.createHash('sha256').update(salted).digest('hex');
+    if (data[0].password != hashed) return(res.status(400).json("Incorrect username or password"));
 
 
     const token = jwt.sign({account_id:data[0].account_id}, "jwtkey") //check id
